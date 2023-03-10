5acdd531b992a28db5e7776e4f6eca173440901f
yazloo <mouhcinebentaleb2000@gmail.com>
2023-03-10 18:39:38 +0100

    swagger

---
+++ src/main/java/ma/sp1/swaggers/SwaggerClass.java
@@ -0,0 +1,17 @@
+package ma.sp1.swaggers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerClass {
+    @Bean
+    public Docket api()
+    {
+        return  new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors
+                .basePackage("ma.sp1.controllers")).build();
+    }
+}
